f5ccd87879de982c673719cdb0f62f756a5b5968
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-10-12 22:51:58 +0300

    feat: add dasboard page

---
+++ pages/dashboard.js
@@ -0,0 +1,16 @@
+export default function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+        </div>
+    );
+}
+
+// export async function getServerSideProps() {
+//     // Fetch data from external API
+//     const res = await fetch(`https://.../data`);
+//     const data = await res.json();
+
+//     // Pass data to the page via props
+//     return { props: { data } };
+// }
