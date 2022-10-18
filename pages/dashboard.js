16183a3f062e71ea422510fdb3f4d889301c21d4
DStukalo <94228608+DStukalo@users.noreply.github.com>
2022-10-19 01:56:43 +0300

    feat: add preloader

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react';
 
 import { Card } from '../components/Card';
 import styles from '../styles/pages/Dashboard.module.scss';
+import LoadingScreen from '../components/Loader';
 
 export default function Dashboard() {
     const [state, setState] = useState(null);
-
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(false);
             const response = await fetch(
                 'https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cbitcoin%2Cbinancecoin%2Cdogecoin%2Cshiba-inu%2Ctron&vs_currencies=usd%2Ceur'
             );
@@ -28,7 +30,7 @@ export default function Dashboard() {
             </h2>
             <section className={styles.cards}>
                 {state && Object.entries(state).map((el) => Card(el))}
-                {!state && <div>Wait few second</div>}
+                {isLoading && <LoadingScreen />}
             </section>
         </div>
     );
